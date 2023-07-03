d4e64a332cc7a236fc7cd7aa9c9c4380c310ff24
skysound102 <skyline_sound@mail.ru>
2023-07-03 22:06:20 +0500

    code change

--- LikePhoto/LikePhotoTests/LikePhotoTests.swift
+++ LikePhoto/LikePhotoTests/LikePhotoTests.swift
@@ -22,16 +22,21 @@ final class LikePhotoTests: XCTestCase {
     }
 
     func testExample() throws {
-        let expectedResult = false
-        var validateResult : Bool
+        fieldValidator.nameField.text! = "Павел"
+        fieldValidator.passwordField.text! = "133"
+        fieldValidator.find(value: fieldValidator.nameField.text!, searchValue2: fieldValidator.passwordField.text!, in:  Base.shared.baseArray)
         
-        fieldValidator.find(value: "Павел", searchValue2: "437813102", in:  Base.shared.baseArray)
+        XCTAssertEqual(fieldValidator.nameField.text!, fieldValidator.person)
+  
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-      
+       
+        
+        
+    
     }
 
     func testPerformanceExample() throws {
